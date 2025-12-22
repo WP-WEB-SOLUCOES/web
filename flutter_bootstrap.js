4b90469ca899ebc75db8ed03bf391e85dfcb2d50
WP WEB SOLUCOES <Wpwebsolucoes@gmail.com>
2025-12-22 16:55:58 -0300

    FInal

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "896623772"
+    serviceWorkerVersion: "3444171799"
   }
 });
