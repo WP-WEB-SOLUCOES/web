a78ab8b95245cce5b1c76eb6d9fd1ba16e9ec484
rafaellima-py <rafael.souza1207@gmail.com>
2026-02-04 11:34:57 -0300

    Correção de token

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "611947186"
+    serviceWorkerVersion: "2545862939"
   }
 });
