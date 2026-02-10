23c459490da3f81646633d73275a1bb2a3b95690
rafaellima-py <rafael.souza1207@gmail.com>
2026-02-10 14:09:52 -0300

    Correção na Orders e Cart Screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3158952326"
+    serviceWorkerVersion: "927583768"
   }
 });
