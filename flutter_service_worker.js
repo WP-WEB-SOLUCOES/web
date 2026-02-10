3bc29ac733f55942002099fc61e40c4f6ac521fd
rafaellima-py <rafael.souza1207@gmail.com>
2026-02-10 13:27:19 -0300

    Correção de teclado

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "0a63bdc4dcceef8cdccd235aa7dfd8ec",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "8d1dacca9d05ee9bcf69b6eaa0e85c86",
+"flutter_bootstrap.js": "ad0b6d7ce15e0fd6503a44bcf46026bf",
 "icons/Icon-192.png": "17ffe7192bc2d666c744e1b2cb638ee9",
 "icons/Icon-512.png": "b7b44b37fa1016e72d8f857ff455f6d0",
 "icons/Icon-maskable-192.png": "17ffe7192bc2d666c744e1b2cb638ee9",
 "icons/Icon-maskable-512.png": "b7b44b37fa1016e72d8f857ff455f6d0",
 "index.html": "b3a18124522d1211fb5349e5da863188",
 "/": "b3a18124522d1211fb5349e5da863188",
-"main.dart.js": "68e759e4802ba1dfd28a2884a3cd53e0",
+"main.dart.js": "69d331d502531b46ad03ea9f0cda4890",
 "manifest.json": "fc16e0c4cce3f9e2c60d6df7448677d2",
 "version.json": "64da48042387ec7be922faae9383821a"};
 // The application shell files that are downloaded before a service worker can
